761dc3bda8b99324eae2ce810a905ca5dae300cb
Eduardo Savian <eduardosavian@outlook.com>
2024-03-10 13:19:32 -0300

    Graph is not working

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import utils as ut
 def main(arg1, arg2):
     input = ut.read_file(arg1)
     matrix = ut.transform_matrix(input)
-    ut.print_matrix(matrix)
+    graph = ut.create_graph_matrix(matrix)
+    ut.print_matrix(graph)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ def read_file(filepath: str) -> str:
     with open(filepath, 'r') as f:
         return f.read()
 
+
 def transform_matrix(input: str) -> list:
     input = input.removeprefix('<').removesuffix('>')
 
@@ -11,14 +12,42 @@ def transform_matrix(input: str) -> list:
             row = []
             for j in range(i, len(input)):
                 if input[j] == '}':
-                    sla = input[i+1:j].split(',')
-                    print(sla)
-                    row.append(input[i+1:j].split(','))
+                    matrix.append(input[i+1:j].split(','))
                     i = j
                     break
-            matrix.append(row)
     return matrix
 
+
 def print_matrix(matrix: list) -> None:
+    print("Matrix:")
+    print(matrix)
+    print("-------")
     for row in matrix:
         print(row)
+
+
+def create_graph_matrix(matrix: list) -> list:
+    graph = []
+    charss = []
+
+    for row in matrix:
+        for col in row:
+            if col not in charss:
+                charss.append(col)
+
+    charss = sorted(set(charss))
+    for i in range(len(charss)):
+        row = []
+        for j in range(len(charss)):
+            if i == j:
+                row.append('X')
+            else:
+                row.append('0')
+        graph.append(row)
+
+    for row in matrix:
+        for i in range(len(row)):
+            for j in range(len(row)):
+                if row[i] != row[j]:
+                    graph[charss.index(row[i])][charss.index(row[j])] = '1'
+    return graph
